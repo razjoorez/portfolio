d1a46a9efb6d7903f3f3dfa3aa877fa56f460446
reza razjoo <reza.razjoo@yahoo.com>
2020-05-23 16:14:04 +0100

    added address module and routing

--- portfolio/src/app/app-routing.module.ts
+++ portfolio/src/app/app-routing.module.ts
@@ -2,7 +2,13 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'personal-details',
+    loadChildren: () => import('./details/details.module').then(m => m.DetailsModule)
+  },
+  { path: 'personal', loadChildren: () => import('./address/address.module').then(m => m.AddressModule) }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
